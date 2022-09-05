eb6841538f197a20f3ea1c8cf2d191804f1b58b2
RD-Diss <112531406+RD-Diss@users.noreply.github.com>
2022-09-05 23:42:20 +0100

    Update network_properties.r

--- network_properties.r
+++ network_properties.r
@@ -9,10 +9,10 @@ library(igraph)
 
 ##Extracting wordlclim data
 
-r <- getData("worldclim",var="bio",res=2.5)
+Polr <- getData("worldclim",var="bio",res=2.5)
 
-r <- r[[c(1,4,12,15)]]
-names(r) <- c("ATemp","TSeas","APrec","PSeas")
+Polr <- Polr[[c(1,4,12,15)]]
+names(Polr) <- c("ATemp","TSeas","APrec","PSeas")
 
 ##Importing network coordinates
 
@@ -22,48 +22,48 @@ Pollat <- c(Polmeta[,8])
 
 Pollon <- c(Polmeta[,9])
 
-coordinates <- data.frame(x=Pollon,y=Pollat)
+coordinates <- data.frame(Long=Pollon,Lat=Pollat)
 
-points <- SpatialPoints(coordinates, proj4string = r@crs)
+points <- SpatialPoints(coordinates, proj4string = Polr@crs)
 
-values <- extract(r,points)
+values <- extract(Polr,points)
 
 PolDat <- cbind.data.frame(coordinates(points),values)
 
 PolDat
 
-plot(r[[1]])
+plot(Polr[[1]])
 plot(points,add=T)
 
 ###Parasite data
 
 ##Extracting wordlclim data
 
-r <- getData("worldclim",var="bio",res=2.5)
+Parr <- getData("worldclim",var="bio",res=2.5)
 
-r <- r[[c(1,4,12,15)]]
-names(r) <- c("ATemp","TSeas","APrec","PSeas")
+Parr <- Parr[[c(1,4,12,15)]]
+names(Parr) <- c("ATemp","TSeas","APrec","PSeas")
 
 ##Importing network coordinates
 
 HParmeta <- read.csv("Parreferences.csv", header = T)
-
+HParmeta
 HParlat <- c(HParmeta[,9])
 
 HParlon <- c(HParmeta[,10])
 
 
-coordinates <- data.frame(x=HParlon,y=HParlat)
+coordinates <- data.frame(Long=HParlon,Lat=HParlat)
 
-points <- SpatialPoints(coordinates, proj4string = r@crs)
+points <- SpatialPoints(coordinates, proj4string = Parr@crs)
 
-values <- extract(r,points)
+values <- extract(Parr,points)
 
-HPardat <- cbind.data.frame(coordinates(points),values)
+HParDat <- cbind.data.frame(coordinates(points),values)
 
-HPardat
+HParDat
 
-plot(r[[1]])
+plot(Parr[[1]])
 plot(points,add=T)
 
 ### Pollinator Network Analysis
@@ -101,7 +101,7 @@ for (i in 1:length(Polnetworks)) {
   PolTotS <- append(PolTotS,S)
   
   PolTotL <- append(PolTotL,L)
-
+  
   PolTotC <- append(PolTotC,C)
   
   PolTotAV <- append(PolTotAV,AV)
@@ -116,7 +116,7 @@ for (i in 1:length(Polnetworks)) {
   PolTotrob <- append(PolTotrob, Polrob)
   
   
-  }
+}
 
 PolTotS
 PolTotL
@@ -127,6 +127,90 @@ PolTotMod
 
 PolTotrob
 
+
 PolNetAn <- data.frame(PolTotS,PolTotL,PolTotAV,PolTotC,PolTotMod,PolTotrob)
 PolNetAn
+
+PolNetAll <-data.frame(PolNetAn,PolDat)
+PolNetAll
+
+### Parasite Network Analysis
+
+##Network Loop 
+
+ParTotS <- c() #Parasite all species number
+ParTotL <- c() #Parasite all links
+ParTotC <- c() #Parasite all connectance
+ParTotAV <- c() #Parasite all average number of links per species
+ParTotMod <- c() #Parasite all modularity
+ParTotrob <- c() #Parasite all robustness
+
+Parnetworks <- list.files("C:\\Users\\danie\\Desktop\\Parasites")
+Parnetworks
+
+for (i in 1:length(Parnetworks)) {
+  
+  net <- as.matrix(read.csv(paste0("C:\\Users\\danie\\Desktop\\Parasites\\",Parnetworks[i]), header = T, row.names = 1))
+  
+  
+  ## Binary Change 
+  net[net != 0] <- 1
+  net
+  
+  #Network Properties
+  S_r <- dim(net)[1]
+  S_c <- dim(net)[2]
+  S <- S_r + S_c
+  L <- sum(net)
+  AV <- L/S
+  C <- L/(S_r * S_c)
+  
+  
+  ParTotS <- append(ParTotS,S)
+  
+  ParTotL <- append(ParTotL,L)
+  
+  ParTotC <- append(ParTotC,C)
+  
+  ParTotAV <- append(ParTotAV,AV)
+  
+  ParMod <- computeModules(net,forceLPA = T)@likelihood #Calculating Modularity
+  
+  ParTotMod <- append(ParTotMod,ParMod)
+  
+  Parex <- second.extinct(net,participant = "both", details = false) 
+  
+  Parrob <- robustness(Parex) #Calculating robustness
+  ParTotrob <- append(ParTotrob, Parrob)
+  
+  
+}
+
+ParTotS
+ParTotL
+ParTotAV
+ParTotC
+
+ParTotMod
+
+ParTotrob
+
+
+ParNetAn <- data.frame(ParTotS,ParTotL,ParTotAV,ParTotC,ParTotMod,ParTotrob)
+ParNetAn
+
+ParNetAll <-data.frame(ParNetAn,HParDat)
+ParNetAll
+
+#modularity
+
+test <- computeModules(net,forceLPA = T)@likelihood
+
+plotModuleWeb(test)
+test
+
+#robustness
+
+robtest <- second.extinct(net,participant = "both", details = false)
+robustness(robtest)
 
