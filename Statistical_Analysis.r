9e50c8d2466678c2b5f5b7e564b5d8944a23bacc
RD-Diss <112531406+RD-Diss@users.noreply.github.com>
2022-09-09 06:10:30 +0100

    Create Statistical_Analysis.r

---
+++ Statistical_Analysis.r
@@ -0,0 +1,20 @@
+### Statistical Analysis Testing
+
+StatTestPol <- read.csv("All_Pollinator_Data.csv" , header = T,)
+
+StatTestPol
+
+LogCon <- log10(StatTestPol$PolTotC)
+LogCon
+
+Annual_Precipitation<- StatTestPol$APrec
+Precipitation_Seasonality <- StatTestPol$PSeas
+Temperature_Seasonality <- StatTestPol$TSeas
+Annual_Temperature <- StatTestPol$ATemp
+
+lineartest1 <- lm(LogCon~Annual_Precipitation+Precipitation_Seasonality+Temperature_Seasonality+Annual_Temperature) 
+
+
+shapiro.test(lineartest1$residuals)
+
+summary(lineartest1)
